2762763e35e506699badb914e96659b1d681701c
Harry B <harry@gethornet.com>
2023-04-09 16:50:50 +0200

    just args log

--- packages/pdfconvert/viags/viags.js
+++ packages/pdfconvert/viags/viags.js
@@ -1,17 +1,6 @@
-const express = require('express');
-const multer = require('multer');
+function main(args) {
+  console.log(args);
+  return { body: 'ok' };
+}
 
-const app = express();
-const upload = multer({ dest: 'uploads/' });
-
-// Define a route that accepts a multipart form data POST request
-app.post('/upload', upload.single('file'), (req, res) => {
-  // Handle the uploaded file here
-  console.log(req.file);
-  res.send('File uploaded successfully');
-});
-
-// Start the server
-app.listen(80, () => {
-  console.log('Server started on port 80');
-});
+exports.main = main;
